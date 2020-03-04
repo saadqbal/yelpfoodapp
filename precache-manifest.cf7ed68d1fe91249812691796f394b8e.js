8d12d7bea54a8c948764ef1549639d97c840a6af
Asad Iqbal <asad.dsoft@gmail.com>
2020-03-04 10:18:14 +0500

    Updates

---
+++ precache-manifest.cf7ed68d1fe91249812691796f394b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dca39874cff969cd631740cfcf99eb",
+    "url": "/yelpfoodapp/index.html"
+  },
+  {
+    "revision": "9e63469cf95fa9cbdc3f",
+    "url": "/yelpfoodapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0598707d82e3e47a77a5",
+    "url": "/yelpfoodapp/static/css/main.033a7a98.chunk.css"
+  },
+  {
+    "revision": "9e63469cf95fa9cbdc3f",
+    "url": "/yelpfoodapp/static/js/2.f96ef846.chunk.js"
+  },
+  {
+    "revision": "0998aa487a46ae21a799d416ae8295e6",
+    "url": "/yelpfoodapp/static/js/2.f96ef846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0598707d82e3e47a77a5",
+    "url": "/yelpfoodapp/static/js/main.f6c1afc3.chunk.js"
+  },
+  {
+    "revision": "ba2d5aab929ca633e4c0",
+    "url": "/yelpfoodapp/static/js/runtime-main.b5b81f13.js"
+  }
+]);
